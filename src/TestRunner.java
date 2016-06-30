37ca910332dc7bc299e87557e3f1a5f71e7cce5a
Illia Rahavoi <illia.rahavoi@nationalleasing.com>
2016-06-30 16:53:46 -0500

    Up

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -4,7 +4,10 @@ import com.rahavoi.util.TextFileReader;
 public class TestRunner {
 	public static void main(String[] args){
 		//new TextFileReader().readFile("/home/irahavoi/workspace/SortableChallenge/src/listings.txt");
+		ProductHandler productHandler = new ProductHandler();
 		new TextFileReader()
-			.readFile("/home/irahavoi/workspace/SortableChallenge/src/products.txt", new ProductHandler());
+			.readFile("/home/irahavoi/workspace/SortableChallenge/src/products.txt", productHandler);
+		
+		System.out.println("Got products: " + productHandler.getProducts().size());
 	}
 }
--- src/com/rahavoi/util/ProductHandler.java
+++ src/com/rahavoi/util/ProductHandler.java
@@ -1,14 +1,34 @@
 package com.rahavoi.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.rahavoi.domain.Product;
 
 public class ProductHandler implements TextFileHandler{
-	private List<Product> products;
+	public static final String PRODUCT_NAME = "product_name";
+	public static final String MANUFACTURER = "manufacturer";
+	public static final String MODEL = "model";
+	public static final String FAMILY = "family";
+	
+	private List<Product> products = new ArrayList<>();
 
 	@Override
 	public void handleTextLine(String textLine) {
+		Product product = new Product();
+		JsonObject json = new JsonParser().parse(textLine).getAsJsonObject();
+		product.setProductName(json.get(PRODUCT_NAME).getAsString());
+		product.setManufacturer(json.get(MANUFACTURER).getAsString());
+		product.setModel(json.get(MODEL).getAsString());
+		
+		JsonElement familyJson = json.get(FAMILY);
+		product.setFamily(familyJson != null ? familyJson.getAs);
+		
+		products.add(product);
+		
 		System.out.println(textLine);
 		
 	}
